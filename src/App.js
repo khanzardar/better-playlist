af26547fe2d637eebdc48ebf179c320178de57c4
khanzardar <k.zardar@gmail.com>
2021-05-01 20:24:53 -0400

    Updated h1 style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+
+let defaultStyle = {
+  color: "purple",
+};
+
+class Aggregate extends Component {
+  render() {
+    return (
+      <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
+        <h2 style={{ defaultStyle }}>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div style={{ defaultStyle }}>
+        <img />
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div style={{ ...defaultStyle, display: "inline-block", width: "25%" }}>
+        <img />
+        <h3>Playlist name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ ...defaultStyle, "font-size": "54px" }}>Title</h1>
+      <Aggregate />
+      <Aggregate />
+      <Filter />
+      <Playlist />
+      <Playlist />
+      <Playlist />
     </div>
   );
 }
